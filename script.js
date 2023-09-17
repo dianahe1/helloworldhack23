227f3c761bc2c152911c110c0a71d2f379e5dd9a
Diana <108499453+dianahe1@users.noreply.github.com>
2023-09-16 23:28:43 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,54 @@
  // Requiring fs module in which
     
-const element = document.getElementById("submitBtn");
-element.addEventListener("click", signUp);
+// const element = document.getElementById("submitBtn");
+// element.addEventListener("click", signUp);
 
-function signUp(){
-    var fname = document.getElementById("fname").value;
-    console.log(fname); 
-    var lname = document.getElementById("lname").value;
-    console.log(lname); 
+// function signUp(){
+
+//     completeMessage.innerHTML = "Thank you for submitting this form!";
+//     document.getElementById("completeMessage").style.color = 'black';
+// }
+
+window.addEventListener("load", function() {
+    const form = document.getElementById('my-form');
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const data = new FormData(form);
+      const action = e.target.action;
+      fetch(action, {
+        method: 'POST',
+        body: data,
+      })
+      .then(() => {
+        completeMessage.innerHTML = "Thank you for submitting this form!";
+      })
+    });
+});
+
 
 
     
 // This variable stores all the data.
-    let data = '\r Name: ' + fname + lname;
+//     let data = '\r Name: ' + fname + lname + email;
     
-    // Convert the text to BLOB.
-    const textToBLOB = new Blob([data], { type: 'text/plain' });
-    const sFileName = 'formData.txt';	   // The file to save the data.
+//     // Convert the text to BLOB.
+//     const textToBLOB = new Blob([data], { type: 'text/plain' });
+//     const sFileName = 'formData.txt';	   // The file to save the data.
 
-    let newLink = document.createElement("a");
-    newLink.download = sFileName;
+//     let newLink = document.createElement("a");
+//     newLink.download = sFileName;
 
-    if (window.webkitURL != null) {
-        newLink.href = window.webkitURL.createObjectURL(textToBLOB);
-    }
-    else {
-        newLink.href = window.URL.createObjectURL(textToBLOB);
-        newLink.style.display = "none";
-        document.body.appendChild(newLink);
-    }
+//     if (window.webkitURL != null) {
+//         newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+//     }
+//     else {
+//         newLink.href = window.URL.createObjectURL(textToBLOB);
+//         newLink.style.display = "none";
+//         document.body.appendChild(newLink);
+//     }
 
-    newLink.click(); 
+//     newLink.click(); 
 
-}
+// }
     
 // writeFile function is defined.
-
